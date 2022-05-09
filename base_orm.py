bc3d7288db456be0136acec121a912d5da4159f0
Matt Nickerson <matt.nickerson993@gmail.com>
2022-05-09 07:43:37 -0400

    basic update

--- base_orm.py
+++ base_orm.py
@@ -6,88 +6,6 @@ from settings import DB_SETTINGS
 from exceptions import ModelNotFound, MultipleObjectsReturned
 from helpers import Column
 
-# class Database:
-#     def __init__(self):
-#         self.conn = psycopg2.connect(
-#             dbname=DB_SETTINGS.get('DB_NAME'),
-#             user=DB_SETTINGS.get('DB_USER'),
-#             password=DB_SETTINGS.get('DB_PASS'),
-#             host=DB_SETTINGS.get('DB_HOST')
-#         )
-
-
-#     def get(self,  **kwargs):
-#         sql, fields, params = model._get_single_row_sql(**kwargs)
-#         cur = self.conn.cursor()
-#         cur.execute(sql, params)
-#         res = cur.fetchall()
-#         num_rows = len(res)
-
-#         if num_rows > 1:
-#             raise MultipleObjectsReturned(f"Call to model manager expected 1 object, call returned {num_rows}!")
-#         elif not num_rows:
-#             raise ModelNotFound('No objects returned from query')
-        
-#         instance = model()
-#         for field, val in zip(fields, res[0]):
-#             setattr(instance, field, val)
-#         return instance
-
-
-# class Model():
-#     def __init__(self, **kwargs):
-#         self._data = {
-#             "id": None
-#         }
-#         self.db = Database()
-#         for key, value in kwargs.items():
-#             self._data[key] = value
-
-#     def __getattribute__(self, key):
-#         _data = super().__getattribute__("_data")
-#         if key in _data:
-#             return _data[key]
-#         return super().__getattribute__(key)
-    
-#     def __setattr__(self, name, value):
-#         super().__setattr__(name, value)
-#         if name in self._data:
-#             self._data[name] = value
-    
-#     @classmethod
-#     def _get_select_all_sql(cls):
-#         SELECT_ALL_SQL = "SELECT {columns} FROM {table_name}s_{table_name};"
-#         table_name = cls.__name__.lower()
-#         cols = ['id']
-#         for name, column_type in inspect.getmembers(cls):
-#             if isinstance(column_type, Column):
-#                 cols.append(name)
-        
-#         sql = SELECT_ALL_SQL.format(columns=", ".join(cols), table_name=table_name)
-#         return sql, cols
-
-
-
-# class Column:
-#     def __init__(self, column_type):
-#         self.type = column_type
-    
-#     @property
-#     def sql_type(self):
-#         SQL_TYPE_MAPPER = {
-#             int: "INTEGER",
-#             float: "REAL",
-#             str: "VARCHAR",
-#             bytes: "BLOB",
-#             bool: "INTEGER"
-#         }
-#         return SQL_TYPE_MAPPER[self.type]
-
-
-# if __name__ == '__main__':
-#     model = Model()
-#     print(model.db.conn)
-
 
 class BaseManager:
 
@@ -158,6 +76,14 @@ class BaseManager:
         cursor.execute(sql, vals)
         res = cursor.fetchone()
         instance._state['id'] = res[0]
+        self._commit()
+        return instance
+    
+
+    def update(self, instance):
+        cursor = self._get_cursor()
+        sql, vals = instance._get_update_sql()
+        cursor.execute(sql, vals)
         self._commit()
         return instance
     
@@ -285,6 +211,26 @@ class Model(metaclass=MetaModel):
         )
     
         return FINAL_SQL, fields, values
+    
+
+    def _get_update_sql(self):
+        UPDATE_SQL = "UPDATE {table_name}s_{table_name} SET {fields} WHERE id = %s"
+        cls = self.__class__
+        cols = []
+        values = []
+        for name, column_type in inspect.getmembers(cls):
+            if isinstance(column_type, Column):
+                cols.append(name)
+                values.append(getattr(self, name))
+        
+        values.append(getattr(self, 'id'))
+
+        FINAL_SQL = UPDATE_SQL.format(
+            table_name=cls.__name__.lower(),
+            fields=", ".join([f"{col} = %s" for col in cols])
+            )
+
+        return FINAL_SQL, values
 
 class Message(Model):
 
@@ -300,10 +246,10 @@ class Message(Model):
 
 if __name__ == "__main__":
     # print(Message.objects)
-    msgs = Message.objects.where( id=1, content='newest content')
-    for msg in msgs:
-        print(msg.id)
-        print(msg.content)
+    # msgs = Message.objects.where( id=1, content='newest content')
+    # for msg in msgs:
+    #     print(msg.id)
+    #     print(msg.content)
     # msgs = Message.objects.all()
     # for msg in msgs:
     #     print(msg.id)
@@ -325,3 +271,13 @@ if __name__ == "__main__":
     # print(msg)
     # print(msg.id)
     # print(msg.content)
+
+    # update
+    msg = Message.objects.get(id=1)
+    print(msg)
+    print(msg.id)
+    print(msg.content)
+    msg.content = 'updated content'
+    new_msg = Message.objects.update(msg)
+    print(new_msg.id)
+    print(new_msg.content)
