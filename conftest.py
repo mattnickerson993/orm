3d0edc258ff6fa8199dd58945e40ed0d944fa4cf
Matt Nickerson <matt.nickerson993@gmail.com>
2022-05-14 09:18:14 -0400

    testing flowgit add .!

--- conftest.py
+++ conftest.py
@@ -20,33 +20,34 @@ from collections import OrderedDict
 
 # destroyed at end of test session -- creates test database and models and drops db at end of session
 @pytest.fixture(scope='session')
-def models():
-    return []
-
-@pytest.fixture(scope='session')
-def test_client_db(models):
-    connection = psycopg2.connect(
-            dbname=DB_SETTINGS.get('DB_NAME'),
-            user=DB_SETTINGS.get('DB_USER'),
-            password=DB_SETTINGS.get('DB_PASS'),
-            host=DB_SETTINGS.get('DB_HOST')
-        )
-    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    cursor = connection.cursor()
-    CREATE_DB_SQL = """CREATE DATABASE test_db;"""
-    cursor.execute(CREATE_DB_SQL)
-    MetaModel.manager_class = BaseManager.set_connection(TEST_DB_SETTINGS)
+def test_client_db(create_test_db, models, test_db_connection):
+    
+    BaseManager.connection = test_db_connection
+    # MetaModel.manager_class = BaseManager
     Job.objects.create_table()
     Message.objects.create_table()
     models.append(Job)
     models.append(Message)
     yield models
     MetaModel.manager_class.connection.close()
-    DELETE_DB_SQL = """DROP DATABASE test_db;"""
-    cursor.execute(DELETE_DB_SQL)
+    drop_test_db()
     return
 
+@pytest.fixture(scope='session')
+def models():
+    return []
 
+@pytest.fixture(scope='session')
+def test_db_connection():
+    connection = psycopg2.connect(
+        dbname=TEST_DB_SETTINGS.get('DB_NAME'),
+        user=TEST_DB_SETTINGS.get('DB_USER'),
+        password=TEST_DB_SETTINGS.get('DB_PASS'),
+        host=TEST_DB_SETTINGS.get('DB_HOST')
+    )
+    return connection
+
+@pytest.fixture(scope='session')
 def create_test_db():
     connection = psycopg2.connect(
             dbname=DB_SETTINGS.get('DB_NAME'),
@@ -58,7 +59,7 @@ def create_test_db():
     cursor = connection.cursor()
     CREATE_DB_SQL = """CREATE DATABASE test_db;"""
     cursor.execute(CREATE_DB_SQL)
-
+    connection.close()
 
 def drop_test_db():
     connection = psycopg2.connect(
@@ -71,3 +72,53 @@ def drop_test_db():
     cursor = connection.cursor()
     DELETE_DB_SQL = """DROP DATABASE test_db;"""
     cursor.execute(DELETE_DB_SQL)
+    return
+
+@pytest.fixture(scope='function')
+def cleanup(test_db_connection):
+    
+    cursor = test_db_connection.cursor()
+    GET_TABLES_QUERY = """
+    SELECT table_name FROM information_schema.tables
+    WHERE table_schema='public' AND table_type='BASE TABLE';
+    """
+    cursor.execute(GET_TABLES_QUERY)
+    res = cursor.fetchall()
+    yield res
+    DELETE_CONTENT_QUERY = "TRUNCATE {table} RESTART IDENTITY CASCADE;"
+    for row in res:
+        try:
+            FINAL_DELETE_SQL = DELETE_CONTENT_QUERY.format(table=row[0])
+            cursor.execute(FINAL_DELETE_SQL)
+        except Exception as e:
+            print(e)
+
+    test_db_connection.commit()
+    return
+
+
+
+# def create_test_db():
+#     connection = psycopg2.connect(
+#             dbname=DB_SETTINGS.get('DB_NAME'),
+#             user=DB_SETTINGS.get('DB_USER'),
+#             password=DB_SETTINGS.get('DB_PASS'),
+#             host=DB_SETTINGS.get('DB_HOST')
+#         )
+#     connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+#     cursor = connection.cursor()
+#     CREATE_DB_SQL = """CREATE DATABASE test_db;"""
+#     cursor.execute(CREATE_DB_SQL)
+
+
+# def drop_test_db():
+#     connection = psycopg2.connect(
+#             dbname=DB_SETTINGS.get('DB_NAME'),
+#             user=DB_SETTINGS.get('DB_USER'),
+#             password=DB_SETTINGS.get('DB_PASS'),
+#             host=DB_SETTINGS.get('DB_HOST')
+#         )
+#     connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+#     cursor = connection.cursor()
+#     DELETE_DB_SQL = """DROP DATABASE test_db;"""
+#     cursor.execute(DELETE_DB_SQL)
--- test_orm.py
+++ test_orm.py
@@ -2,7 +2,7 @@ import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import pytest
 from base_orm import Message
-from exceptions import ModelNotFound
+from exceptions import ModelNotFound, MultipleObjectsReturned
 # from base_orm import Job, Message
 
 from settings import DB_SETTINGS
@@ -10,7 +10,7 @@ from settings import DB_SETTINGS
 
 
 
-def test_create_models(test_client_db):
+def test_create_models(test_client_db, cleanup):
     Job, Message = test_client_db
     job = Job.objects.create(data='test data')
     msg = Message.objects.create(content='test content')
@@ -19,8 +19,10 @@ def test_create_models(test_client_db):
     assert msg.content == 'test content'
     assert job.data == 'test data'
 
-def test_get_models(test_client_db):
+def test_get_models(test_client_db, cleanup):
     Job, Message = test_client_db
+    Job.objects.create(data='test data')
+    Message.objects.create(content='test content')
     job = Job.objects.get(id = 1)
     msg = Message.objects.get(id = 1)
     assert job.id == 1
@@ -28,7 +30,55 @@ def test_get_models(test_client_db):
     assert msg.content == 'test content'
     assert job.data == 'test data'
 
-def test_model_not_found_exception(test_client_db):
+def test_model_not_found_exception(test_client_db, cleanup):
     Job, _ = test_client_db
     with pytest.raises(ModelNotFound):
         job = Job.objects.get(id=100)
+
+
+def test_multi_objs_exception(test_client_db, cleanup):
+    Job, _ = test_client_db
+    Job.objects.create(data='test data')
+    Job.objects.create(data='test data')
+    with pytest.raises(MultipleObjectsReturned):
+        job = Job.objects.get(data='test data')
+
+
+def test_all(test_client_db, cleanup):
+    Job, _ = test_client_db
+    Job.objects.create(data='test data')
+    Job.objects.create(data='more test data')
+    jobs = Job.objects.all()
+    assert len(jobs) == 2
+
+
+def test_where(test_client_db, cleanup):
+    Job, Message = test_client_db
+    Job.objects.create(data='test data')
+    Message.objects.create(content='test content')
+    Message.objects.create(content='test content')
+    msgs = Message.objects.where(content = 'test content')
+    ids = [msg.id for msg in msgs]
+    assert ids == [1, 2]
+
+def test_save(test_client_db, cleanup):
+    Job, Message = test_client_db
+    job = Job(
+        data = 'data to save'
+    )
+    job.save()
+    assert job.id == 1
+    assert job.data == 'data to save'
+
+def test_delete(test_client_db, cleanup):
+    Job, _ = test_client_db
+    job = Job(
+        data = 'data to save'
+    )
+    job.save()
+    assert job.id == 1
+    assert job.data == 'data to save'
+    job.delete()
+    with pytest.raises(ModelNotFound):
+        job = Job.objects.get(id=1)
+    return
